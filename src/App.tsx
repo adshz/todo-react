4eaa27e10293f961a7a086317b4aa15605396c56
shenghongzhong <howdavidlearncoding@gmail.com>
2025-02-10 17:17:57 +0000

    [adshz] Add Prettier and ESLint configuration, refactor Todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from "react";
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+const TodoList = ({ todos, onDelete }: { todos: Todo[], onDelete: (id: number) => void }) => (
+  <ul className="todo-list">
+    {todos.map((todo) => (
+      <TodoItem 
+        key={todo.id} 
+        todo={todo} 
+        onDelete={onDelete} 
+      />
+    ))}
+  </ul>
+);
+
+const TodoItem = ({ todo, onDelete }: { todo: Todo, onDelete: (id: number) => void }) => (
+  <div className="todo">
+    <li>{todo.text}</li>
+    <button 
+      className="delete-button" 
+      onClick={() => onDelete(todo.id)}
+    >
+      Delete
+    </button>
+  </div>
+);
+
+const EmptyTodo = () => (
+  <div className="empty">
+    <p>No task found</p>
+  </div>
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  
+  const addTodo = (): void => {
+    if (todo.trim() === '') {
+      return ;
+    }
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: todo,
+      completed: false
+    };
+
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+    setTodo('');
+  };
+
+  const deleteTodo = (id: number): void => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>React ToDo App</h1>
+      <div className="input-wrapper">
+        <input 
+          type="text" 
+          name="todo" 
+          placeholder="Add a new todo" 
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button className="add-button" onClick={addTodo}>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      
+      {todos.length > 0 
+        ? <TodoList todos={todos} onDelete={deleteTodo} />
+        : <EmptyTodo />
+      }
+    </div>
+  );
 }
 
 export default App
